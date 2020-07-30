4f1902375f0285f8f456d0b1f9399e799c929358
Dmitri L <larionovdmitri@gmail.com>
2020-07-30 09:09:14 -0500

    adding first example for switch map and in process of config for typescript node env

---
+++ SwitchMap/demo.ts
@@ -0,0 +1,40 @@
+import { of, Observable, OperatorFunction, SchedulerLike } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+
+
+export class SwitchMapRxJsDemo {
+    constructor(
+        public arrayOfNumbers = [1, 2, 3]
+    ) {
+
+        //perform the operations
+        let subject = this.ofOperatorResult(arrayOfNumbers)
+
+        //pipe the switch logic handler
+        subject.pipe(this.switchLogicHandler);
+
+        //subscribe to the results
+        subject.subscribe(
+            this.handlePushFromSubscription
+        );
+    }
+    ofOperatorResult = (numberCollection: number[]): Observable<number> => {
+        return of(...numberCollection)
+    }
+    switchMapHandler = (x: number, index: number): Observable<number> => {
+        let one = x
+        let two = x ** 2
+        let three = x ** 3
+        console.log(`Switch Map Handler Index: ${index} number: ${x}`)
+        return this.getOfHandler([one, two, three])
+    }
+    getOfHandler = (collection: (number | SchedulerLike)[]): Observable<number> => {
+        return of(...collection)
+    }
+    switchLogicHandler: OperatorFunction<number, number> = switchMap(this.switchMapHandler)
+    handlePushFromSubscription = (number: number): void => console.log(`Of Operator Result number value of ${number}`)
+}
+
+
+//execute the demo
+export const switchMapRxJsDemo = new SwitchMapRxJsDemo()
---
+++ demos.md
@@ -0,0 +1,20 @@
+# RxJS Demos
+
+To best understand the surface area of RxJS it's best to use examples to demonstrate each tool in the tool set.
+
+# Typescript
+
+To best understand how RxJs works under the hood, all examples have explicit typing.
+
+[TS Transpilation Config](https://www.typescriptlang.org/docs/handbook/tsconfig-json.html)
+
+## Observable
+
+This is the basic unit in RxJS, and it relies on Promises for it's existence.
+
+## [Switch Map](https://www.learnrxjs.io/learn-rxjs/operators/transformation/switchmap)
+
+[Docs](https://rxjs.dev/api/operators/switchMap)
+
+
+
